468863ab3625cd6178c330d2d931cd0885057880
Oussama_Achour <oachour08@gmail.com>
2023-12-24 16:25:28 +0100

    Update root component : importing children components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ChildAComponent } from './child-a/child-a.component';
+import { EmojiComponent } from './emoji/emoji.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,ChildAComponent,EmojiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/emoji/emoji.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import "bootstrap/dist/css/bootstrap.css" 
+@Component({
+  selector: 'app-emoji',
+  standalone: true,
+  imports: [],
+  templateUrl: './emoji.component.html',
+  styleUrl: './emoji.component.css'
+})
+export class EmojiComponent {
+
+}
