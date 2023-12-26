93a887dd8dd21ebcf356e9c1879eac0a0f1658e3
Oussama_Achour <oachour08@gmail.com>
2023-12-26 18:18:18 +0100

    feat: adding event handler to emoji component

--- src/app/emoji/emoji.component.ts
+++ src/app/emoji/emoji.component.ts
@@ -10,8 +10,21 @@ import "bootstrap/dist/css/bootstrap.css"
 export class EmojiComponent {
 Title= "Oussama"
 status=true
+// inputElement = new HTMLInputElement ;
+
 changeStatus(){
   console.log("hello")
   this.status=!(this.status)
 }
+
+// method to unpack the event object and gain insights
+handleInputChange(event: Event): void {
+  // Cast the event to HTMLInputElement to access input-specific properties
+  const inputElement = event.target as HTMLInputElement;
+  // Access the input value
+  const inputValue = inputElement.value;
+  console.log('Input value:', inputValue);
+  // You can add your logic here based on the input value
+}
+
 }
