2f140cc814324c34ffe9ef657032c4f1f4834c78
Oussama_Achour <oachour08@gmail.com>
2023-12-26 21:26:36 +0100

    feat: adding a task form

--- src/app/child-a/child-a.component.ts
+++ src/app/child-a/child-a.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { GrandChildAComponent } from '../grand-child-a/grand-child-a.component';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-child-a',
   standalone: true,
-  imports: [GrandChildAComponent],
+  imports: [GrandChildAComponent,FormsModule, NgFor],
   templateUrl: './child-a.component.html',
   styleUrl: './child-a.component.css'
 })
 export class ChildAComponent {
-title="this is ChildA component"
+title="List of items"
+// parent to child
 currentItem="this is from ChildA"
+// child to parent : receiving an item 
+userInput: string = '';
+itemList: string[] = ["hello","world","x"];
+
+addItem() {
+  if (this.userInput.trim() !== '') {
+    this.itemList.push(this.userInput);
+    console.log(this.userInput)
+    this.userInput = '';
+  }
+  
+}
+
+removeItem(index: number) {
+  this.itemList.splice(index, 1);
+}
+
 }
